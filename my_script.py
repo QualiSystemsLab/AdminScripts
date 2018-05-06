04cd0b6a8249f57771437863858ab88edd9ca29c
Natti Katz <Natti.k@qualisystems.com>
2018-05-06 15:28:00 +0300

    passing json succesfully to python

--- my_script.py
+++ my_script.py
@@ -1,1 +1,33 @@
-print('python running from node')
+import sys
+import json
+
+print('python running from nodeJS')
+# print "\n".join(sys.argv)
+
+#Read data from stdin
+def read_in():
+    # lines = sys.stdin.readlines()
+    load = sys.stdin.read()
+    # Since our input would only be having one line, parse our JSON data from that
+    # return json.loads(lines[0])
+    return json.loads(load)
+
+def main():
+    #get our data as an array from read_in()
+    lines = read_in()
+    print(lines)
+
+    # Sum  of all the items in the providen array
+    # total_sum_inArray = 0
+    # for item in lines:
+        # total_sum_inArray += item
+
+    #return the sum to the output stream
+    # print total_sum_inArray
+
+
+# Start process
+if __name__ == '__main__':
+    main()
+
+
--- server.js
+++ server.js
@@ -1,29 +1,54 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
+const PythonShell = require('python-shell');
 
+
+const mockObj = {
+  param1: "firstParameter",
+  param2: "secondParameter"
+}
 
 //// ===================== python shell ===========================
 
-var PythonShell = require('python-shell');
-var pyshell = new PythonShell('my_script.py');
+function runPyShell(jsonString='{"param1":"yo"}') {
+  var shell = new PythonShell('my_script.py', { mode: 'text' });
 
-// sends a message to the Python script via stdin
-pyshell.send('hello');
+  var options = {
+    mode: 'text',
+    args: ['my First Argument', 'My Second Argument']
+  };
 
-pyshell.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement)
-  console.log(message);
-});
+  // sends a message to the Python script via stdin
+  // shell.send('hello');
+  // shell.send(mockJSON);
 
-// end the input stream and allow the process to exit
-pyshell.end(function (err, code, signal) {
-  if (err) throw err;
-  console.log('The exit code was: ' + code);
-  console.log('The exit signal was: ' + signal);
-  console.log('finished');
-  console.log('finished');
-});
+  // shell.send(JSON.stringify([1, 2, 3, 4, 5]));
+  mockJSON = JSON.stringify(mockObj);
+  console.log('mockJSON from Node: ', mockJSON);
+  // shell.send(mockJSON);
+  shell.send(jsonString);
+
+  shell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log('onMessage: ', message);
+  });
+
+  // end the input stream and allow the process to exit
+  shell.end(function (err, code, signal) {
+    if (err) throw err;
+    console.log('The exit code was: ' + code);
+    console.log('The exit signal was: ' + signal);
+    console.log('finished');
+  });
+}
+
+// Passing arguments
+// PythonShell.run('my_script.py', options, function (err, results) {
+//   if (err) throw err;
+//   // results is an array consisting of messages collected during execution
+//   console.log('results: %j', results);
+// });
 
 
 //// ===================== server ===========================
@@ -38,8 +63,16 @@ pyshell.end(function (err, code, signal) {
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post("/testing", function (request, response) {
-      console.log(request.body); //This prints the JSON document received (if it is a JSON document)
+  console.log(request.body); //This prints the JSON document received (if it is a JSON document)
+  console.log(typeof (request.body));
+  requestString = JSON.stringify(request.body);
+  console.log(requestString);
+  console.log(typeof(requestString));
+
+  runPyShell(requestString);
+
 });
 
 app.get('/', (req, res) => res.send('Hello World!'))
